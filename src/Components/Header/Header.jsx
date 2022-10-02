2403a08c6a2e8f9dc6286f26c217c5e94956e723
Tassio Med <tassiohfmed@gmail.com>
2022-10-01 21:24:28 -0300

    feat: componente Header criado e configurado nas routes

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+function Header(){
+  return(
+    <header>
+      <h1>Header</h1>
+    </header>
+  )
+}
+
+export default Header;
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Movie from './Pages/Movie/Movie';
 
+import Header from './Components/Header/Header';
+
 function RoutesApp(){
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={ <Home/> }/>
         <Route path="/movie:id" element={ <Movie/> }/>
