222ce15dd3d37bc84775b636e7e90210a1b78016
Tassio Med <tassiohfmed@gmail.com>
2022-10-02 11:43:23 -0300

    feat: função baseURl do axios criada

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,10 @@
+import axios from 'axios';
+
 // API URL: /tv/popular?api_key=fedd1180bea199228b93bece9b17a8df&language=pt-BR
-//BASE DA API: https://api.themoviedb.org/3/
+//BASE DA URL: https://api.themoviedb.org/3/
+
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/'
+});
+
+export default api;
