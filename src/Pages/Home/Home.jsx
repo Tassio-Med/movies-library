dbf35725b2fcf7a156cbfeae08762f3722dc5f63
Tassio Med <tassiohfmed@gmail.com>
2022-10-03 15:43:12 -0300

    requisição da api na Home feita

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,26 @@
+import React from "react";
+import { useState, useEffect } from 'react';
+import api from '../../services/api';
+
+// API URL: /tv/popular?api_key=fedd1180bea199228b93bece9b17a8df&language=pt-BR
+
 function Home(){
+  const [filmes, setFimes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes(){
+      const response = await api.get("tv/popular", {
+        params: {
+          api_key:"fedd1180bea199228b93bece9b17a8df",
+          language: "pt-BR",
+          page: 1,
+        }
+      })
+      console.log(response.data.results);
+    }
+    loadFilmes();
+  }, [])
+
   return(
     <div className="w-full h-screen uppercase flex flex-col relative top-20">
       <h1 >Bem-vindo</h1>
