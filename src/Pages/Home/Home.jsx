a5d0b90e40e030059e5d8561808c7a4ffc5abe50
Tassio Med <tassiohfmed@gmail.com>
2022-10-03 17:09:54 -0300

    feat: chamda de filmes ok, design temporário

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useState, useEffect } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom'
 
 // API URL: /tv/popular?api_key=fedd1180bea199228b93bece9b17a8df&language=pt-BR
 
 function Home(){
-  const [filmes, setFimes] = useState([]);
+  const [filmes, setFilmes] = useState([]);
 
   useEffect(() => {
     async function loadFilmes(){
@@ -16,16 +17,27 @@ function Home(){
           page: 1,
         }
       })
-      // console.log(response.data.results.slice(0, 15));
-      setFimes(response.data.results.slice(0, 15))
-      
+      console.log(response.data.results.slice(0, 15));
+      setFilmes(response.data.results.slice(0, 10))
+
     }
     loadFilmes();
   }, [])
 
   return(
     <div className="w-full h-screen uppercase flex flex-col relative top-20">
-      <h1 >Bem-vindo</h1>
+      <h1>Olá mundo</h1>
+      <div className="w-20 flex justify-between items-center m-16">
+        {filmes.map((filme) => {
+          return(
+            <div key={filme.id} >
+              <h2 className="text-black">{filme.name}</h2>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.name} />
+              <button><Link to={`/filme/${filme.id}`}>Acessar</Link></button>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
