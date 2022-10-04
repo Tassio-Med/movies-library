9c9f0386135d50d5f9aaab233e20ed66fad701d1
Tassio Med <tassiohfmed@gmail.com>
2022-10-04 18:02:52 -0300

    feat: background, botão e logo melhorados.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,13 +3,13 @@ import {BiCameraMovie, BiStar} from 'react-icons/bi'
 
 function Header(){
   return(
-    <header className='w-screen h-20 shadow-bottom z-20 fixed drop-shadow-md flex justify-between items-center'>
-      <div className='mx-5'>
+    <header className='w-screen h-24 bg-gradient-to-b from-gray-900 shadow-bottom z-20 fixed drop-shadow-md flex justify-between items-center'>
+      <div className='mx-10'>
         <Link to="/" className=''>
-          <BiCameraMovie className='text-5xl text-indigo-800 md:text:3xl'/>
+          <BiCameraMovie className='text-5xl text-indigo-600 md:text:3xl'/>
         </Link>
       </div>
-      <button className='w-30 h-10 mx-5 px-4 py-1 bg-indigo-600 rounded-lg flex items-center hover:bg-indigo-800'>
+      <button className=' tracking-wider w-36 h-10 mx-10 px-4 py-1 bg-indigo-600 rounded-lg flex items-center hover:bg-indigo-800'>
         <Link to="/favoritos" className='w-full h-full font-bold text-slate-50 flex items-center'>Favoritos</Link>
         <BiStar className='text-2xl text-slate-50'/>
       </button>
