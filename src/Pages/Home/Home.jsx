a9e980cec5d7341fb1d25074b0cdd1e603760c81
Tassio Med <tassiohfmed@gmail.com>
2022-10-04 18:36:00 -0300

    feat: arquivo com os códigos da paǵina Carousel

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,133 @@
-import React from "react";
-import Carousel from "./Carousel";
+/* eslint-disable react-hooks/exhaustive-deps */
+// App.js
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Slider from "react-slick";
 
+import {RiStarFill, RiFlagFill} from 'react-icons/ri'
+import {FaCalendarAlt} from 'react-icons/fa'
+
+import api from '../../services/api';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// API URL: /tv/popular?api_key=fedd1180bea199228b93bece9b17a8df&language=pt-BR
 
 function Home(){
-  return(
-    <div className="App">
-      <Carousel/>
+  const [filmes, setFilmes] = useState([]);
+
+  const [nav1, setNav1] = useState(null);
+  const [nav2, setNav2] = useState(null);
+  const [slider1, setSlider1] = useState(null);
+  const [slider2, setSlider2] = useState(null);
+
+  useEffect(() => {
+    setNav1(slider1);
+    setNav2(slider2);
+  }, []); 
+
+  useEffect(() => {
+    async function loadFilmes(){
+      const response = await api.get("tv/popular", {
+        params: {
+          api_key:"fedd1180bea199228b93bece9b17a8df",
+          language: "pt-BR",
+          page: 1,
+        }
+      })
+      // console.log(response.data.results.slice(0, 15));
+      setFilmes(response.data.results.slice(0, 10))
+
+    }
+    loadFilmes();
+  }, [])
+
+
+  const settingsMain = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  };
+
+  const settingsThumbs = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    swipeToSlide: true,
+    focusOnSelect: true,
+    centerPadding: '20px'
+  };
+
+  return (
+    <div className=" w-full h-full uppercase  relative overflow-x-hidden">
+       <div className="slider-wrapper">  
+
+        {/* CAPA */}
+
+        <div className='relative z-0'>
+          <Slider
+            {...settingsMain}
+            asNavFor={nav2}
+            ref={slider => (setSlider1(slider))}
+            className="w-full absolute"
+          >
+            {filmes.map((filme) =>
+              <div className="slick-slide" key={filme.id}>
+                <div className='w-[800px] h-full p-14 text-white absolute insent-0  z-30 block justify-start align-center'>
+                  <div className='text-shadow w-full p-3'>
+                    <h2 className="slick-slide-title text-6xl font-bold drop-shadow-3xl font-">{filme.name}</h2>
+                    <div className='flex justify-between w-80  my-4 text-lg'>
+                      <h3 className='mx-1 flex items-center drop-shadow-3xl'><RiStarFill className='mx-1 text-yellow-400'/> {filme.vote_average}</h3>
+                      <h3>|</h3>
+                      <h3 className='mx-1 flex items-center drop-shadow-3xl'><FaCalendarAlt className='mx-2 text-indigo-600'/>{filme.first_air_date}</h3>
+                      <h3>|</h3>
+                      <h3 className='mx-1 flex items-center drop-shadow-3xl'><RiFlagFill className='mx-1 text-blue-600'/>{filme.origin_country}</h3>
+                    </div>
+                    <button className='w-36 h-10 px-4 py-1 rounded-lg bg-indigo-600  hover:bg-indigo-800 flex justify-center items-center tracking-wider'>
+                      <Link className="font-bold text-slate-50 flex items-center" to={`/filme/${filme.id}`} > Acessar </Link>
+                    </button>
+                  </div>
+                </div>
+                <div className='w-full' key={filme.id}>
+                  <img className="w-full h-full" src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.name} />
+                </div>
+              </div>
+            )}
+          </Slider>
+          {/* THUMBNAIL */}
+          <div className="thumbnail-slider-wrap absolute inset-0 z-10 w-full h-full left-16">
+            <Slider
+              {...settingsThumbs}
+              asNavFor={nav1}
+              ref={slider => (setSlider2(slider))}
+              className="w-[550px] m-10 grid-rows-3 card-blur"
+              >
+
+              {filmes.map((filme) =>
+
+                <div className="card p-full m-5 " key={filme.id}>
+                  {/* <h2 className="text-lg font-bold">{filme.name}</h2> */}
+                  <img className="w-full h-full object-cover rounded-xl" src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.name}/>
+                
+                </div>
+
+              )}
+
+            </Slider>
+          </div>
+        </div>
+        
+      </div>
+     
     </div>
-  )
+  );
 }
 
+
+ 
 export default Home;
