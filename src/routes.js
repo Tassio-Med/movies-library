89c2922e6fb24b8a8ca84db2005e7986085d5b2b
Tassio Med <tassiohfmed@gmail.com>
2022-10-05 16:43:14 -0300

    feat: rota de erro configurada

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Movie from './Pages/Movie/Movie';
 
+import Error from './Pages/Error/Error';
+
 import Header from './Components/Header/Header';
 
 function RoutesApp(){
@@ -11,7 +13,8 @@ function RoutesApp(){
       <Header/>
       <Routes>
         <Route path="/" element={ <Home/> }/>
-        <Route path="/filme:id" element={ <Movie/> }/>
+        <Route path="/filme/:id" element={ <Movie/> }/>
+        <Route path="*" element={ <Error/> }/>
       </Routes>
     </BrowserRouter>
   )
