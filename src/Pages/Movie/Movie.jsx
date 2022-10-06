329de2dd1ff2cc0ea1cfa7a2b787d6cec99c990a
Tassio Med <tassiohfmed@gmail.com>
2022-10-06 11:51:48 -0300

    fix: erro de rota corrigido

--- src/Pages/Movie/Movie.jsx
+++ src/Pages/Movie/Movie.jsx
@@ -1,7 +1,57 @@
+import React from "react";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import api from '../../services/api';
+
 function Movie(){
+  const { id } = useParams();
+
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    async function loadFilmes(){
+      await api.get(`/tv/${id}`, {
+        params:{
+          api_key:"fedd1180bea199228b93bece9b17a8df",
+          language: "pt-BR",
+        }
+      })
+      .then((response)=>{
+        setFilme(response.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        console.log('Filme não encontrado');
+      })
+    }
+
+    loadFilmes();
+
+    return () => {
+      console.log("O componente foi desmontado")
+    }
+  }, [])
+
+  if(loading){
+    return(
+      <div className='w-full flex justify-center align-center mt-20 absolute bg-green-400'>
+        <h2>Carregando detalhes</h2>
+      </div>
+    )
+  }
+
+
   return(
     <div>
-      <h1>Págia de filmes</h1>
+      <h1>{filme.name}</h1>
+      <img className="w-full h-full" src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.name} />
+
+      <h3>Sinopse</h3>
+      <div>{filme.overview}</div>
+
+      <strong>Nota: {filme.vote_average}</strong>
     </div>
   )
 }
