90198b10045f9e87fbf0f78f4b13e635d592fa16
Tassio Med <tassiohfmed@gmail.com>
2022-10-06 17:02:40 -0300

    feat: estrutura da páǵina Favorites criada

--- src/Pages/Favorites/Favorites.jsx
+++ src/Pages/Favorites/Favorites.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Favorites(){
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const myMoviesList = localStorage.getItem("@movielibrary");
+    setMovies(JSON.parse(myMoviesList) || [])
+  }, [])
+
   return(
     <div>
-      <h1>Página da Favoritos</h1>
+      <h1>Página de Favoritos</h1>
+
+      <ul>
+        {movies.map((item)=>{
+          return(
+            <li key={item.id}>
+              <span>{item.name}</span>
+              <div>
+                <button>
+                  <Link to={`/movie/${item.id}`}>Ver detalhes</Link>
+                </button>
+                <button>Excluir</button>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
